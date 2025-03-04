19ea7ea40e9544f8d9b07728f7f3e3b4c6be323d
Victor Hermez <20391088+VictorHermez@users.noreply.github.com>
2025-03-03 16:32:48 -0800

    DB_PASS update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ TOKEN = os.getenv("TOKEN")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASS = os.getenv("DB_PASS")
 
 # Setup intents and bot
 intents = discord.Intents.default()
@@ -31,7 +31,7 @@ def connect_to_database():
             host=DB_HOST,
             database=DB_NAME,
             user=DB_USER,
-            password=DB_PASSWORD
+            password=DB_PASS
         )
         return connection
     except mysql.connector.Error as err:
